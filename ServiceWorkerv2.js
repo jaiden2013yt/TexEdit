d92a52edd41a89e9f5e724d0d189a936c375d606
Jaiden2013YT <jkjaiden2013@gmail.com>
2026-04-27 10:31:59 -0700

    Update ServiceWorkerv2.js

--- ServiceWorkerv2.js
+++ ServiceWorkerv2.js
@@ -1,3 +1,13 @@
+const PopulateCache = async (fileList) => {
+  const cache = await caches.open("v0d1")
+	await cache.addAll(fileList)
+}
+
+self.addEventListener("install", (event) => {
+  PopulateCache(["index.html",])
+
+})
+
 self.addEventListener("fetch", (event) => {
   console.log("Handling fetch event for", event.request.url);
 });
