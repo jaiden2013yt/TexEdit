c75d8a2cfb77830b0bcc41f1646d59ee8ac14096
Jaiden2013YT <jkjaiden2013@gmail.com>
2026-04-27 17:46:36 -0700

    Update ServiceWorkerv2.js

--- ServiceWorkerv2.js
+++ ServiceWorkerv2.js
@@ -57,7 +57,7 @@ self.addEventListener("fetch",  (event) => {
 		const cachedAsset = await caches.match(event.request)
 		if(cachedAsset === undefined) {
 			try {
-				if(corsAllowedURLs.includes(event.request.url)) {
+				if(corsAllowedURLs.includes(event.request.url) || event.request.destination === "font") {
 					return await fetch(event.request)
 				} else {
 					return await fetch(event.request, { mode: 'no-cors'})
